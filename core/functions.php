6366f30ac83fe3402728c86c2bdf2f429363ad06
ravindrawan <132208871+ravindrawan@users.noreply.github.com>
2026-03-24 06:31:26 +0530

    Update functions.php

--- core/functions.php
+++ core/functions.php
@@ -1,1 +1,16 @@
+<?php
+// core/functions.php
 
+if (!function_exists('checkRateLimit')) {
+    function checkRateLimit($pdo, $ip, $action, $limit, $minutes) {
+        // දැනට error එක නැති වෙන්න මෙතනින් true යවමු.
+        // ඔයාට ඇත්තටම rate limit කරන්න ඕනෙ නම් පස්සේ මේකට logic එකක් ලියමු.
+        return true; 
+    }
+}
+
+if (!function_exists('clearRateLimit')) {
+    function clearRateLimit($pdo, $ip, $action) {
+        return true;
+    }
+}
