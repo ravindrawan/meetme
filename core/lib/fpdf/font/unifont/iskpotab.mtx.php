720e81c1cf8600cc8310e35e75973d6401aa3b2f
ravindrawan <132208871+ravindrawan@users.noreply.github.com>
2026-06-07 22:58:59 +0530

    Add files via upload

---
+++ core/lib/fpdf/font/unifont/iskpotab.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='IskoolaPota-Bold';
+$type='TTF';
+$desc=array (
+  'Ascent' => 913.0,
+  'Descent' => -242.0,
+  'CapHeight' => 662.0,
+  'Flags' => 262148,
+  'FontBBox' => '[-587 -249 1931 898]',
+  'ItalicAngle' => 0.0,
+  'StemV' => 165.0,
+  'MissingWidth' => 492.0,
+);
+$up=-93;
+$ut=44;
+$ttffile='C:\xampp\htdocs\vms/font/unifont/iskpotab.ttf';
+$originalsize=361136;
+$fontkey='iskoolaB';
+?>
